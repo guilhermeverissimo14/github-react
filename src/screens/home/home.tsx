50326e4d660cb9d33429ddd32218741851d06c46
José Guilherme <guilhermeverissimo14@gmail.com>
2023-09-20 09:47:15 -0300

    finalizando a estilização da página de home

--- src/screens/home/home.tsx
+++ src/screens/home/home.tsx
@@ -1,7 +1,23 @@
-export default function Home(){
-    return(
-        <div>
-            <h1>Home</h1>
-        </div>
+import { useNavigate } from 'react-router-dom';
+import './home.css'
+
+export default function Home() {
+    const navigate = useNavigate();
+
+    function search(){
+        navigate("/profile")
+    }
+
+    return (
+        <main className="main-home">
+            <div className="title">
+                <h1 className="title-search">Search</h1>
+                <h1 className="title-devs">d_evs</h1>
+            </div>
+            <div className='content-search'>
+                <input type="text" placeholder='search' />
+                <button onClick={search}>search</button>
+            </div>
+        </main>
     )
 }
